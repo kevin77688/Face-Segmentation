2299226387d63abca088f105c2a02de899be959b
Kevin Duan <kevin77688@gmail.com>
2023-12-12 00:26:43 +0800

    Add CBAM as attention

---
+++ model/cbam.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ChannelAttention(nn.Module):
+    def __init__(self, num_channels, reduction_ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(num_channels, num_channels // reduction_ratio, bias=False),
+            nn.ReLU(),
+            nn.Linear(num_channels // reduction_ratio, num_channels, bias=False)
+        )
+
+    def forward(self, x):
+        avg_out = self.fc(self.avg_pool(x).view(x.size(0), -1))
+        max_out = self.fc(self.max_pool(x).view(x.size(0), -1))
+        out = avg_out + max_out
+        return F.sigmoid(out).view(x.size(0), x.size(1), 1, 1)
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        padding = kernel_size // 2
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return F.sigmoid(x)
+
+class CBAM(nn.Module):
+    def __init__(self, num_channels, reduction_ratio=16, kernel_size=5):
+        super(CBAM, self).__init__()
+        self.channel_attention = ChannelAttention(num_channels, reduction_ratio)
+        self.spatial_attention = SpatialAttention(kernel_size)
+
+    def forward(self, x):
+        x = x * self.channel_attention(x)
+        x = x * self.spatial_attention(x)
+        return x
---
+++ model/efficientnet_cbam_unet.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+from efficientnet_pytorch import EfficientNet
+from torchvision import models
+from .cbam import CBAM
+
+class DecoderBlock(nn.Module):
+    def __init__(self, in_channels, middle_channels, out_channels):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels, middle_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            CBAM(middle_channels),
+            nn.Conv2d(middle_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
+class Model(nn.Module):
+    def __init__(self, n_channels=3, n_classes=19, encoder_name='efficientnet-b4'):
+        super().__init__()
+        self.encoder = EfficientNet.from_pretrained(encoder_name)
+
+        self.decoder1 = DecoderBlock(1792, 1792, 448)
+        self.decoder2 = DecoderBlock(448, 448, 160)
+        self.decoder3 = DecoderBlock(160, 160, 56)
+        self.decoder4 = DecoderBlock(56, 56, 32)
+        self.decoder5 = DecoderBlock(32, 32, 24)
+
+        self.cbam_final = CBAM(24)
+
+        self.out_conv = nn.Conv2d(24, n_classes, kernel_size=1)
+
+    def forward(self, x):
+        # Encoder
+        features = self.encoder.extract_features(x)
+
+        # Decoder
+        x = self.decoder1(features)
+        x = self.decoder2(x)
+        x = self.decoder3(x)
+        x = self.decoder4(x)
+        x = self.decoder5(x)
+
+        # CBAM
+        x = self.cbam_final(x)
+
+        # Output
+        x = self.out_conv(x)
+        return x
